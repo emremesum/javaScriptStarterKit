6596c4eeb99c9861a9ed81a770a31365d0422f06
EmreMesum <emremesum@gmail.com>
2021-11-22 00:48:39 +0300

    jsLesson

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("merhaba kodlama.io");
+
+let konutkredileri = [12, "emre konut", ["a", "b", "c"]]
+
+console.log(konutkredileri)
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+}
